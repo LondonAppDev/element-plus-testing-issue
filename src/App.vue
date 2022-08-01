3eb1793d3656a9ce449adaa208f37b278bc1cb88
Mark Winterbottom <mark@londonappdeveloper.com>
2022-08-01 11:10:27 +0100

    new project with list

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>Names List</h1>
+  <names-list :tableData="names"></names-list>
+</template>
+
+<script lang="ts" setup>
+import NamesList from "./components/NamesList.vue";
+const names = [{ name: "Nala" }, { name: "Simba" }];
+</script>
---
+++ src/components/NamesList.vue
@@ -0,0 +1,15 @@
+<template>
+  <el-table :data="props.tableData">
+    <el-table-column prop="name" label="Name"></el-table-column>
+  </el-table>
+</template>
+
+<script lang="ts" setup>
+import { ElTable, ElTableColumn } from "element-plus";
+const props = defineProps({
+  tableData: {
+    type: Array,
+    required: true,
+  },
+});
+</script>
